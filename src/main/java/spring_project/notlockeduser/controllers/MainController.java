7a84cc9b107823c9137c2675addef26fc03f0421
Bogdan <58120983+notlockeduser@users.noreply.github.com>
2021-06-08 19:30:54 +0300

    lesson 20
    Add calculator

--- src/main/java/spring_project/notlockeduser/controllers/MainController.java
+++ src/main/java/spring_project/notlockeduser/controllers/MainController.java
@@ -1,18 +1,60 @@
 package spring_project.notlockeduser.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
     @GetMapping("/home")
-    public String getHomePage(){
+    public String getCalculatorPage() {
+
         return "main/home";
     }
 
+    @GetMapping("/calculator")
+    public String getCalculatorPage(Model model,
+                                    @RequestParam(value = "num1", required = false) String num1,
+                                    @RequestParam(value = "num2", required = false) String num2,
+                                    @RequestParam(value = "oper", required = false) String operation) {
+
+        String response = null;
+        int number1, number2;
+        if (num1 == null || num2 == null) {
+            response = "Repeat your request";
+        } else {
+            number1 = Integer.parseInt(num1);
+            number2 = Integer.parseInt(num2);
+            switch (operation) {
+                case "add":
+                    response = number1 + " + " + number2 + " = " +
+                            (number1 + number2);
+                    break;
+                case "sub":
+                    response = number1 + " - " + number2 + " = " +
+                            (number1 - number2);
+                    break;
+                case "div":
+                    response = number1 + " / " + number2 + " = " +
+                            (number1 / number2);
+                    break;
+                case "mul":
+                    response = number1 + " * " + number2 + " = " +
+                            (number1 * number2);
+                    break;
+                default:
+                    response = "Repeat your request";
+                    break;
+            }
+        }
+
+        model.addAttribute("message", response);
+        return "main/calculator";
+    }
+
     @GetMapping("/about")
-    public String getAboutPage(){
+    public String getAboutPage() {
         return "main/about";
     }
 }
