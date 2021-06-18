a917802c103579d90a4469a65e86554bd39097aa
Bogdan <58120983+notlockeduser@users.noreply.github.com>
2021-06-18 19:07:05 +0300

    lesson 25
    JDBC API. PostgreSQL. Add method to show all from database

--- src/main/java/spring_project/notlockeduser/DAO/PersonDAO.java
+++ src/main/java/spring_project/notlockeduser/DAO/PersonDAO.java
@@ -1,44 +1,75 @@
 package spring_project.notlockeduser.DAO;
 
+import org.postgresql.Driver;
+import org.postgresql.jdbc.PgConnection;
 import org.springframework.stereotype.Component;
 import spring_project.notlockeduser.models.Person;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
     static int ID = 0;
-    List<Person> listPeople = new ArrayList<>();
+    //List<Person> listPeople = new ArrayList<>();
 
-    public PersonDAO() {
-        listPeople.add(new Person(ID++, "Bob", "t@mail", 27));
-        listPeople.add(new Person(ID++, "Tob", "twr@mail", 23));
-        listPeople.add(new Person(ID++, "Billy","321@mail", 22));
-        listPeople.add(new Person(ID++, "Ugly", "adfa@mail", 28));
+    static String URL = "jdbc:postgresql://localhost:5432/firstDB";
+    static String USERNAME = "postgres";
+    static String PASSWORD = "postgre";
+
+    static Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<Person> getIndex() {
-        return listPeople;
+        List<Person> people = new ArrayList<>();
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+            String SQLRequest = "SELECT * FROM person";
+            ResultSet resultSet = statement.executeQuery(SQLRequest);
+
+            while (resultSet.next()) {
+                Person person = new Person();
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setAge(resultSet.getInt("age"));
+                person.setEmail(resultSet.getString("email"));
+                people.add(person);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return people;
     }
 
     public Person getPerson(int id) {
-        System.out.println(listPeople.get(id));
-        return listPeople.get(id);
+//        System.out.println(listPeople.get(id));
+//        return listPeople.get(id);
+        return null;
     }
 
     public void add(Person person) {
-        person.setId(ID++);
-        listPeople.add(person);
+//        person.setId(ID++);
+//        listPeople.add(person);
     }
 
     public void update(Person person, int id) {
-        listPeople.get(id).setName(person.getName());
-        listPeople.get(id).setEmail(person.getEmail());
-        listPeople.get(id).setAge(person.getAge());
+//        listPeople.get(id).setName(person.getName());
+//        listPeople.get(id).setEmail(person.getEmail());
+//        listPeople.get(id).setAge(person.getAge());
     }
 
     public void delete(int id) {
-        listPeople.remove(id);
+//        listPeople.remove(id);
     }
 }
